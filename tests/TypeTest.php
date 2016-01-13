2aca180be0a826c22de141e49990f8d9ff12714e
Atahanasios Alexandris <thanosalexander@gmail.com>
2016-01-13 19:42:52 +0200

    adding tests

--- tests/TypeTest.php
+++ tests/TypeTest.php
@@ -5,29 +5,28 @@ use Thanosalexander\Activity\Exceptions\Type\NullDataException;
 
 class TypeTest extends TestsBase
 {
-    /**
-     * @var Type
-     */
-    private $type;
-
-    /**
-     * TypeTest constructor.
-     */
-    public function setUp()
-    {
-        parent::setUp();
-
-        $this->type = new Type();
-    }
 
     public function test_it_does_not_create_type_if_data_is_null()
     {
         $data = null;
 
-        $this->type->create($data);
+        $new_type = (new Type())->create($data);
 
+        $this->assertInstanceOf(NullDataException::class,$new_type);
     }
 
+    public function test_it_does_not_create_type_if_data_keys_are_wrong()
+    {
+        $data = [
+            'name'=>'like',
+            'label'=>'Like',
+            'wrong_field'=>'this is wrong'
+        ];
+
+        $new_type = (new Type())->create($data);
+
+        $this->assertInstanceOf(NullDataException::class,$new_type);
+    }
 
 
 
